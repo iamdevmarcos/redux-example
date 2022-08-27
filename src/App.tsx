2409994dd1c2131b9b45f6295a9b0b6720f983eb
Marcos Andre <marcosdev.me@gmail.com>
2022-08-27 17:50:54 -0300

    add github corner

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import Wrapper from 'components/Wrapper'
 import ThemeExample from 'examples/ThemeExample'
 import TodoExample from 'examples/TodoExample'
 
+import GithubCorner from 'react-github-corner'
+
 const App = () => (
   <Wrapper>
-    {/* <ThemeExample /> */}
+    <ThemeExample />
     <TodoExample />
+
+    <GithubCorner
+      href="https://github.com/iamdevmarcos/redux-example"
+      octoColor="#FBFBFB"
+      bannerColor="#0449F7"
+      target="_blank"
+    />
   </Wrapper>
 )
 
