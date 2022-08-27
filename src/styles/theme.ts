0f767d1ec7a063101f29e4b0902ad15ec6031488
Marcos Andre <marcosdev.me@gmail.com>
2022-08-27 13:22:26 -0300

    add theme declaration

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components"
+
+export const DarkTheme: DefaultTheme = {
+  mode: 'dark',
+  colors: {
+    background: 'yellow'
+  }
+}
+
+export const LightTheme: DefaultTheme = {
+  mode: 'light',
+  colors: {
+    background: 'blue'
+  }
+}
